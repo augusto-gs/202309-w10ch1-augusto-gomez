33f8ed1ad9a4bbfcefc42ce44f573dc365971846
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-13 20:58:09 +0100

    Add middleware to delete specified object

--- src/features/thingsLearned/routers/thingsLearnedRouter.ts
+++ src/features/thingsLearned/routers/thingsLearnedRouter.ts
@@ -11,7 +11,25 @@ thingsLearnedRouter.get("/", (_req, res) => {
 thingsLearnedRouter.get("/:idThing", (req, res) => {
   const { idThing } = req.params;
   const thingsElement = thingsLearned.find((thing) => thing.id === +idThing);
+
+  if (!thingsElement) {
+    res.status(400).json({ error: "Incorrect id" });
+    return;
+  }
+
   res.status(200).json({ thingsElement });
+});
+
+thingsLearnedRouter.delete("/:idThing", (req, res) => {
+  const { idThing } = req.params;
+  const thingsIndex = thingsLearned.findIndex((thing) => thing.id === +idThing);
+  thingsLearned.splice(thingsIndex, 1);
+
+  if (thingsIndex === -1) {
+    res.status(400).json({ error: "Thing not found" });
+  }
+
+  res.status(200).json({});
 });
 
 thingsLearnedRouter.use(urlNotFound);
